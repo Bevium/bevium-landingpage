9c62c4792d74ccf6e20242970c4d48ce90c459a4
federico-arona <federico.arona.94@gmail.com>
2026-05-12 15:43:45 +0200

    add configs and action for building and deploying

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const repo = 'bevium-landingpage'
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
