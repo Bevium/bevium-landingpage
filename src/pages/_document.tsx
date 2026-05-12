e6ad57007072909fbe6d2e07c63de2dc36b47164
federico-arona <federico.arona.94@gmail.com>
2026-05-12 16:37:28 +0200

    fix favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href={publicPath('/images/logo/Bevium_iconlogo_redblack.png')}/>
+        <link rel="icon" href={publicPath('/images/logo/Bevium_iconlogo_redwhite_32_32.png')}/>
         <meta
           name="description"
           content="Bevium: Technical partner for complex software projects with focus on AI integration and interactive/3D software. Senior engineers integrated directly into your team."
